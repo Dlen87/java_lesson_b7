c61c04e36923235e9d73816bd85a1d6eb4acef46
Dlen87 <dlen87@mail.ru>
2019-08-10 02:18:13 +0200

    Добавлен класс PointTests для тестирования функционала класса Point

--- sandbox/src/test/java/by/stqa/lesson1/sandbox/PointTests.java
+++ sandbox/src/test/java/by/stqa/lesson1/sandbox/PointTests.java
@@ -1,8 +1,16 @@
 package by.stqa.lesson1.sandbox;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class PointTests {
 
-    Point p1 = new Point(1,1);
-    Point p2 = new Point(2,1);
+    @Test
+    public  void testPoint() {
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(2, 1);
+
+        Assert.assertEquals(p1.distance(p2), 1.0);
+    }
 
 }
