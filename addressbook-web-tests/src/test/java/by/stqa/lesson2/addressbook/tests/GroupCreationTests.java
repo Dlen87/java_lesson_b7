0739a0c435c089fbab0ce6d8384d829a251bb8a3
Dlen87 <dlen87@mail.ru>
2019-08-14 15:53:35 +0200

    Добавили класс GroupHelper
    в нем содержатся методы для работы с группами

--- addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupCreationTests.java
@@ -8,9 +8,9 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreation() throws Exception {
         app.gotoGroupPage();
-        app.initGroupCreation();
-        app.fillGroupForm(new GroupData("test10", "test122", "test1222"));
-        app.submitGroupCreation();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData("test10", "test122", "test1222"));
+        app.getGroupHelper().submitGroupCreation();
         app.returnToGroupPage();
         app.loginOut();
     }
--- addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupDeletionTests.java
@@ -7,8 +7,8 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.gotoGroupPage();
-    app.selectGroup();
-    app.deleteSelectedGroups();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deleteSelectedGroups();
     app.returnToGroupPage();
     app.loginOut();
   }
