316f4aeeeefea4be1a02eb93d2853f3fbd502aa6
Dlen87 <dlen87@mail.ru>
2019-08-15 16:12:05 +0200

    Создан класс GroupModificationTest для тестов модификации групп

---
+++ addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package by.stqa.lesson2.addressbook.tests;
+
+import by.stqa.lesson2.addressbook.modal.GroupData;
+import org.testng.annotations.Test;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification(){
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().modificationSelectGroup();
+        app.getGroupHelper().fillGroupForm((new GroupData("testAAA", "testAAA", "testAAA")));
+        app.getGroupHelper().updateSelectGroup();
+        app.getNavigationHelper().returnToGroupPage();
+    }
+}
