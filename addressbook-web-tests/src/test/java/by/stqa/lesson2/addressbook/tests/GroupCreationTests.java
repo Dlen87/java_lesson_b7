cc88c92705be1f83ef897a36596b165db497fb9c
Dlen87 <dlen87@mail.ru>
2019-08-30 11:32:08 +0200

    Формирование коллекции из элементов групп

--- addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupCreationTests.java
@@ -4,15 +4,17 @@ import by.stqa.lesson2.addressbook.modal.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().creationGroup(new GroupData("test0", null, null));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().creationGroup(new GroupData("test6", null, null));
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
     }
 
 
