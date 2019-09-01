ffdebc9805d9f3a7a8460304d90418545a9dcecd
Dlen87 <dlen87@mail.ru>
2019-09-01 15:09:14 +0200

    Использование лямбда вырожения при поиске максимального Id

--- addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupCreationTests.java
@@ -4,8 +4,11 @@ import by.stqa.lesson2.addressbook.modal.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
+
+//import static jdk.vm.ci.sparc.SPARC.o1;
 
 public class GroupCreationTests extends TestBase {
 
@@ -13,18 +16,14 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test999", null, null);
+        GroupData group = new GroupData("test9", null, null);
         app.getGroupHelper().creationGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         //Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (GroupData element : after){
-            if (element.getId() > max){
-                max = element.getId();
-            }
-        }
-        group.setId(max);
+        Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+        int max1 = after.stream().max(byId).get().getId();
+        group.setId(max1);
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
 
