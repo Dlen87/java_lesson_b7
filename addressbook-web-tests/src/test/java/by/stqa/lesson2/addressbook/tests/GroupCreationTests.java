1e4838c2b27ab23969e0ce2b025e90fc64384f78
Dlen87 <dlen87@mail.ru>
2019-09-01 23:54:12 +0200

    Сортировка списков с помощью лямбда выражения

--- addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupCreationTests.java
@@ -16,16 +16,15 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test9", null, null);
+        GroupData group = new GroupData("test879", null, null);
         app.getGroupHelper().creationGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         //Assert.assertEquals(after.size(), before.size() + 1);
-
-        Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
-        int max1 = after.stream().max(byId).get().getId();
-        group.setId(max1);
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
+        Comparator<? super GroupData> byId =  (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(after,before);
 
     }
 
