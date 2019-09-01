b22ff39d240ff27f8b725c20af1d5b62532c5a20
Dlen87 <dlen87@mail.ru>
2019-09-01 14:41:31 +0200

    Добавлена проверка на корректность при изменении группы

--- addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import by.stqa.lesson2.addressbook.modal.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -17,10 +18,15 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().modificationSelectGroup();
-        app.getGroupHelper().fillGroupForm((new GroupData("test9", null, null)));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test9", null, null);
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().updateSelectGroup();
         app.getGroupHelper().returnToGroupPage();
+
         List<GroupData>  after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(), before.size());
+      //  Assert.assertEquals(after.size(), before.size());
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(after),new HashSet<Object>(before));
     }
 }
