312f52f647b1c6f483c5c715deeb776f3275abeb
Dlen87 <dlen87@mail.ru>
2019-09-01 14:37:16 +0200

    Добавлена проверка на корректность при создании новой группы, устанавливаем правильный id

--- addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import by.stqa.lesson2.addressbook.modal.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -12,9 +13,21 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().creationGroup(new GroupData("test6", null, null));
+        GroupData group = new GroupData("test999", null, null);
+        app.getGroupHelper().creationGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(), before.size() + 1);
+        //Assert.assertEquals(after.size(), before.size() + 1);
+
+        int max = 0;
+        for (GroupData element : after){
+            if (element.getId() > max){
+                max = element.getId();
+            }
+        }
+        group.setId(max);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
+
     }
 
 
