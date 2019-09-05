64082fca39af1de8dc3eff184cc6383493c7e3d3
Dlen87 <dlen87@mail.ru>
2019-09-05 02:10:45 +0200

    Реализация проверки путём сравнения списков при модификации контакта

--- addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/tests/ContactModificationTests.java
@@ -16,15 +16,15 @@ public class ContactModificationTests extends TestBase {
             app.getContactHelper().creationContact(new ContactData(null, null, "Test1", null, null, null, null, null, null, null, null, "1", "January", "1900", "test0"));
         }
         List<ContactData> before = app.getContactHelper().getContactList();
-        app.getContactHelper().selectedContacts(0);
+        app.getContactHelper().selectedContacts(before.size()-1);
         app.getContactHelper().modificationSelectedContact();
-        ContactData contact = new ContactData(before.get(0).getId(),"SVNet", null, "SVTetina", null, null, null, null, "1003456789", "putin@mail.ru", "Moscow", "123", "15", "March", "1986", null);
+        ContactData contact = new ContactData(before.get(before.size()-1).getId(),"aSVNet", null, "aSVTetina", null, null, null, null, "1003456789", "putin@mail.ru", "Moscow", "123", "15", "March", "1986", null);
         app.getContactHelper().fillContactCreation(contact, false);
         app.getContactHelper().updateSelectedContact();
         app.getContactHelper().returnToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
       //  Assert.assertEquals(after.size(), before.size());
-        before.remove(0);
+        before.remove(before.size()-1);
       //  Thread.sleep(250);
         before.add(contact);
         Comparator<? super ContactData> byId = (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
