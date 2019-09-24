a9963a539d9a05939cabcdcf43b736ca8129124c
Dlen87 <dlen87@mail.ru>
2019-09-24 16:53:22 +0300

    Инициализируем класс DbHelper

--- addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/appmanager/ApplicationManager.java
@@ -21,6 +21,7 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser=browser;
@@ -30,6 +31,8 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = properties.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+        dbHelper =new DbHelper();
 
         if (browser.equals(BrowserType.FIREFOX)){
             wd = new FirefoxDriver();
@@ -66,4 +69,6 @@ public class ApplicationManager {
     public NavigationHelper goTo() {
         return navigationHelper;
     }
+
+    public DbHelper db(){return dbHelper;}
 }
