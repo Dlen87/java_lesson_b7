79ce3fc1116bdc3ba34634d564be8023d84ec78b
Dlen87 <dlen87@mail.ru>
2019-09-24 16:52:40 +0300

    Добавлен помощник для выгрузки данных из базы данных

---
+++ addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/appmanager/DbHelper.java
@@ -0,0 +1,44 @@
+package by.stqa.lesson2.addressbook.appmanager;
+
+import by.stqa.lesson2.addressbook.modal.ContactData;
+import by.stqa.lesson2.addressbook.modal.Contacts;
+import by.stqa.lesson2.addressbook.modal.GroupData;
+import by.stqa.lesson2.addressbook.modal.Groups;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class DbHelper {
+
+    private final SessionFactory sessionFactory;
+
+    public DbHelper(){
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    public Groups groups(){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<GroupData> result = session.createQuery( "from GroupData" ).list();
+        session.getTransaction().commit();
+        session.close();
+        return new Groups(result);
+    }
+
+    public Contacts contacts(){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<ContactData> result = session.createQuery( "from ContactData" ).list();
+        session.getTransaction().commit();
+        session.close();
+        return new Contacts(result);
+    }
+}
