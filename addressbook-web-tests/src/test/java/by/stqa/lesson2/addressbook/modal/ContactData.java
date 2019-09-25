92e07c8935ebbff971fbe927d5e7a2955ec9df7a
Dlen87 <dlen87@mail.ru>
2019-09-26 01:39:47 +0300

    Добавлена аннотация @Transient для поля photo

--- addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/modal/ContactData.java
+++ addressbook-web-tests/src/test/java/by/stqa/lesson2/addressbook/modal/ContactData.java
@@ -77,7 +77,6 @@ public class ContactData {
     @Expose
     @Transient
     private String byear;
-
     @Expose
     @Transient
     private String group;
@@ -85,9 +84,7 @@ public class ContactData {
     private String allphones;
     @Transient
     private String allemails;
-
-    @Column(name = "photo")
-    @Type(type = "text")
+    @Transient
     private String  photo;
 
     public ContactData withPhoto(File photo) {
@@ -276,12 +273,15 @@ public class ContactData {
         ContactData that = (ContactData) o;
         return id == that.id &&
                 Objects.equals(firstname, that.firstname) &&
-                Objects.equals(lastname, that.lastname);
+                Objects.equals(lastname, that.lastname) &&
+                Objects.equals(address, that.address) &&
+                Objects.equals(mobile, that.mobile) &&
+                Objects.equals(email, that.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstname, lastname);
+        return Objects.hash(id, firstname, lastname, address, mobile, email);
     }
 
     @Override
