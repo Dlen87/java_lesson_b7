cbf292324fcf223192dd22604da2823996ed0d31
Dlen87 <dlen87@mail.ru>
2019-10-05 02:33:35 +0300

    Изменения связанные с сервером непрерывной интеграции

---
+++ sandbox/src/test/java/by/stqa/lesson1/sandbox/SquareTest.java
@@ -0,0 +1,14 @@
+package by.stqa.lesson1.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTest {
+
+    @Test
+    public void testArea(){
+        int a = 2;
+        int square = a * a;
+        Assert.assertEquals(square, 4);
+    }
+}
