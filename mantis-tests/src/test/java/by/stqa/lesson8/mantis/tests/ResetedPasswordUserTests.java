3c35531475b98f95854b0e46253fed9b660aed56
Dlen87 <dlen87@mail.ru>
2019-10-06 23:17:24 +0300

    Запуск внешнего почтового сервера для mantis

--- mantis-tests/src/test/java/by/stqa/lesson8/mantis/tests/ResetedPasswordUserTests.java
+++ mantis-tests/src/test/java/by/stqa/lesson8/mantis/tests/ResetedPasswordUserTests.java
@@ -1,26 +1,47 @@
 package by.stqa.lesson8.mantis.tests;
 
+import by.stqa.lesson8.mantis.appmanager.HttpSession;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
 
 public class ResetedPasswordUserTests extends TestBase {
     private WebDriver wd;
 
     @Test
     public void testResetPasswordUser() throws Exception {
-        wd.get("http://localhost/mantisbt-2.22.0/login_page.php");
-        wd.findElement(By.xpath("//input[@id='username']")).click();
-        wd.findElement(By.xpath("//input[@id='username']")).clear();
-        wd.findElement(By.xpath("//input[@id='username']")).sendKeys("administrator");
-        wd.findElement(By.xpath("//input[@value='Войти']")).click();
-        wd.findElement(By.xpath("//input[@id='password']")).click();
-        wd.findElement(By.xpath("//input[@id='password']")).clear();
-        wd.findElement(By.xpath("//input[@id='password']")).sendKeys("secret");
-        wd.findElement(By.xpath("//input[@value='Войти']")).click();
-        wd.findElement(By.linkText("user")).click();
+        goToLoginPage();
+      //  String username = "administrator";
+       // String locator = "username";
+        loginToMantis( "username", "administrator");
+        loginToMantis( "password", "secret");
+        controlUsers();
+        selectUser();
+        resetPassword();
+    }
+
+    private void resetPassword() {
         wd.findElement(By.xpath("//input[@value='Сбросить пароль']")).click();
-        wd.findElement(By.linkText("Продолжить")).click();
+    }
+
+    private void selectUser() {
+        wd.findElement(By.linkText("user2")).click();
+    }
+
+    private void controlUsers() {
         wd.findElement(By.linkText("Управление пользователями")).click();
+    }
+
+    private void loginToMantis(String locator, String key) {
+        wd.findElement(By.xpath("//input[@id='" + locator + "']")).click();
+        wd.findElement(By.xpath("//input[@id='" + locator + "']")).clear();
+        wd.findElement(By.xpath("//input[@id='" + locator + "']")).sendKeys(key);
+        wd.findElement(By.xpath("//input[@value='Войти']")).click();
+    }
+
+    private void goToLoginPage() {
+        wd.get("http://localhost/mantisbt-2.22.0/login_page.php");
     }
 }
